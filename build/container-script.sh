1414bef0dc4a13a36b7152d22ce2053edfd4ecea
TherLaf <therlaf0409@gmail.com>
2026-04-17 20:57:47 +0800

    Updated compose, build, and container script.

--- build/container-script.sh
+++ build/container-script.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 # Add container setup bash code here...
+cat > /home/Steam/update_zomboid.txt <<'EOL'
+// update_zomboid.txt
+//
+@ShutdownOnFailedCommand 1 //set to 0 if updating multiple servers at once
+@NoPromptForPassword 1
+force_install_dir /opt/pzserver/
+//for servers which don't need a login
+login anonymous
+app_update 380870 validate
+quit
+EOL
