5dd1267c636e67febb4891c403ec01c473505105
TherLaf <therlaf0409@gmail.com>
2026-04-23 00:14:18 +0800

    Implemented a steamcmd server update trigger through env.

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -4,12 +4,12 @@ SERVER_EXECUTABLE="/home/steam/server-files/start-server.sh"
 
 export PATH=$PATH:/usr/games
 
-# Download server files if needed.
-if [ ! -f $SERVER_EXECUTABLE ]; then
+# Download server files if needed or triggered.
+if [[ ! -f $SERVER_EXECUTABLE || $update_server_files = "true" ]]; then
   echo "Downloading server files..."
 
   bash /home/steam/steamcmd/steamcmd.sh +runscript /home/steam/update_zomboid.txt
 fi
 
 # Start the server.
-bash $SERVER_EXECUTABLE -servername $servername
+bash $SERVER_EXECUTABLE -servername $server_name
